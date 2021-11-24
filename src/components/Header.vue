07f03f54782650d06b15d10c53b84476d7dbb683
Manuel Guariglia <manuelguariglia@gmail.com>
2021-11-24 15:18:42 +0100

    FIX HEADER AND MAIN

---
+++ src/components/Header.vue
@@ -0,0 +1,91 @@
+<template>
+  <header>
+    <div class="container">
+      <div>
+        <img src="../assets/img/dc-logo.png" alt="">
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#"><span>Home</span></a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data(){
+    return{
+      links:[
+        {
+          url:'/characters',
+          text: 'CHARACTERS',
+          current: false
+        },
+        {
+          url:'/comics',
+          text: 'COMICS',
+          current: true
+        },
+        {
+          url:'/movies',
+          text: 'MOVIES',
+          current: false
+        },
+        {
+          url:'/tv',
+          text: 'TV',
+          current: false
+        },
+        {
+          url:'/games',
+          text: 'GAMES',
+          current: true
+        },
+        {
+          url:'/collectibles',
+          text: 'COLLECTIBLES',
+          current: false
+        },
+        {
+          url:'/videos',
+          text: 'VIDEOS',
+          current: false
+        },
+        {
+          url:'/fans',
+          text: 'FANS',
+          current: true
+        },
+        {
+          url:'/news',
+          text: 'NEWS',
+          current: false
+        },
+        {
+          url:'/shop',
+          text: 'SHOP',
+          current: false
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  header{
+    height: 90px;
+    width: 100vw;
+    background-color: white;
+  }
+
+  
+  .container img{
+    width: 70px;
+  }
+  
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <div class="container">
+      <p>--> Content goes here </p>
+    </div>
+  </main>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  main{
+    height: 90px;
+    width: 100vw;
+    background-color: black;
+    color: white;
+  }
+  
+</style>
