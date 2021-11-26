802cd4cf66eec629f05a5d5999da75731b5f5235
Manuel Guariglia <manuelguariglia@gmail.com>
2021-11-26 19:37:14 +0100

    fix jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header :menu = "linkedList"/>
+    <Jumbotron />
     <Main />
     <Info />
     <Footer />
@@ -9,6 +10,7 @@
 
 <script>
 import Header from './components/Header.vue'
+import Jumbotron from './components/Jumbotron.vue'
 import Main from './components/Main.vue'
 import Info from './components/Info.vue'
 import Footer from './components/Footer.vue'
@@ -17,6 +19,7 @@ export default {
   name: 'App',
   components: {
     Header,
+    Jumbotron,
     Main,
     Info,
     Footer
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="jumbotron">
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Jumbotron',
+  data(){
+    return{
+    } 
+  }
+}      
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../assets/style/mixins.scss';
+  .jumbotron{
+    background-image: url('../assets/img/jumbotron.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100vw;
+    height: 300px;
+  }
+  
+</style>
